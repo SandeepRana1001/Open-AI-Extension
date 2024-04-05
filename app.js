36bdba7c99a2bd81d76070c65d97767d8dc0ebbb
Sandeep Rana <38207659+SandeepRana1001@users.noreply.github.com>
2024-04-05 14:34:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 const generateCode = document.getElementById('generateCode');
-const prompt_text_area = document.getElementById('prompt')
-const prompt = prompt_text_area.value.trim();
+const prompt_input_box = document.getElementById('prompt')
+const success_message = document.getElementById('success-message');
 
-// prompt_text_area.addEventListener('keyup', (e) => {
-//     const value = e.target.value.trim();
-//     const counter = document.getElementById('min-words_counter');
-//     console.log(value);
-//     if (value.length > 0) {
-//         if (value.length < 11) {
-//             counter.innerHTML = value.length;
-//         }
-//     } else {
-//         counter.innerHTML = 0;
-//     }
+prompt_input_box.addEventListener('keyup', (e) => {
+    const value = e.target.value.trim();
+    const counter = document.getElementById('min-words_counter');
+    if (value.length > 0) {
+        if (value.length < 11) {
+            counter.innerHTML = value.length;
+        }
+    } else {
+        counter.innerHTML = 0;
+    }
 
-// })
+})
 
 generateCode.addEventListener('click', async () => {
     const error_message_holder = document.getElementById('error-message_holder');
     const error = document.getElementById('error-message');
-    console.log(prompt_text_area)
-    copyData(prompt)
+    const prompt = prompt_input_box.value.trim();
 
-    // if (prompt.length == 0) {
-    //     error_message_holder.innerText = 'Please provide a valid prompt';
-    //     error.style.visibility = 'none';
-    // } else if (prompt.length < 10) {
-    //     error_message_holder.innerText = 'Prompt must be at least 10 characters';
-    //     error.style.visibility = 'none';
-    // } else {
-    //     error_message_holder.innerText = '';
-    //     error.style.visibility = 'hidden';
-    //     await copyData(prompt)
-    // }
+    if (prompt.length == 0) {
+        error_message_holder.innerText = 'Please provide a valid prompt';
+        error.style.opacity = 1;
+    } else if (prompt.length < 10) {
+        error_message_holder.innerText = 'Prompt must be at least 10 characters';
+        error.style.opacity = 1;
+    } else if (prompt.length > 10) {
+        error_message_holder.innerText = '';
+        error.style.opacity = 0;
+        await fetchResponseFromGPT(prompt)
+    }
 });
 
 const copyData = async (data) => {
     try {
 
         navigator.clipboard.writeText(data);
-        // alert('Copied text: ' + data);
+        success_message.style.opacity = 1
+        setTimeout(() => {
+            success_message.style.opacity = 0;
 
+        }, 2000)
     } catch (err) {
-        console.log(err)
     }
 }
 
 
 const fetchResponseFromGPT = async (prompt) => {
 
-    var key = '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';
+    var auth_header = 'Bearer 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';
     var body = {
         "model": "gpt-3.5-turbo",
         "messages": [
             {
                 "role": "system",
-                "content": "You are a coding assistant, skilled in providing code for complex coding scenarios"
+                "content": "You are a coding assistant, skilled in providing code for complex coding scenarios.You will only provide code, no other information. Only code and no other string and no other information"
             },
             {
                 "role": "user",
@@ -67,19 +67,13 @@ const fetchResponseFromGPT = async (prompt) => {
         method: 'POST', // or 'PUT'
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${key}`
+            'Authorization': auth_header
         },
         body: JSON.stringify(body),
     }
     const request = await fetch('https://api.openai.com/v1/chat/completions', payload);
     const response = await request.json();
-    console.log(response);
     const output = response.choices[0].message.content;
     console.log(output);
+    await copyData(output);
 }
-// async function get() {
-//     await fetchResponseFromGPT('Tell me about Javascript in 200 words')
-
-// }
-
-// get()
